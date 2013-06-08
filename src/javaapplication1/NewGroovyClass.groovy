553443c4b12dc1c0bc11202375fd779f3c68103e
kazuhito_m <kazuhito.sumpic@gmail.com>
2013-06-08 15:21:07 +0900

    日本語は通るかな？出力された文言を変更。

--- src/javaapplication1/NewGroovyClass.groovy
+++ src/javaapplication1/NewGroovyClass.groovy
@@ -1,12 +1,13 @@
 package javaapplication1
 
 /**
- *
+ *にほんごご
  * @author kazuhito
  */
 class NewGroovyClass {
     static void main(String[] s) {
         println 'miura'
+        println 'stadying git command ! lets Develop!'
     }
 }
 
